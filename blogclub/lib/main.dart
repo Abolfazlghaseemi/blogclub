8cf04e233f5ae02c8cf3e4f26c4957efbc2bc049
Abolfazl.ghaseemi <ariancode@yahoo.com>
2023-12-30 01:31:50 +0330

    The app is  update

--- blogclub/lib/main.dart
+++ blogclub/lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
     const primaryColor = Color(0xff376AED);
 
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         textButtonTheme: TextButtonThemeData(
